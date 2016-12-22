21e31ccaa746809e2dff1545c42e60e05617a04f
encid <ryancav@gmail.com>
2016-12-22 09:15:31 -0500

    updated gifts to For loop

--- Daily-Challenge-296/Program.cs
+++ Daily-Challenge-296/Program.cs
@@ -7,50 +7,22 @@ namespace DailyChallenge296 {
         static void Main()
         {
             var Days = new List<string> { "first", "second", "third", "fourth", "fifth", "sixth",
-                                "seventh", "eighth", "ninth", "tenth", "eleventh", "twelveth" };
+                        "seventh", "eighth", "ninth", "tenth", "eleventh", "twelveth" };
+
+            var Gifts = new List<string> { "a Partridge in a Pear Tree", "two Turtle Doves", "three French Hens",
+                        "four Calling Birds", "five Golden Rings", "six Geese a Laying", "seven Swans a Swimming",
+                        "eight Maids a Milking", "nine Ladies Dancing", "ten Lords a Leaping", "eleven Pipers Piping",
+                        "twelve Drummers Drumming" };
 
             for (int i = 1; i <= 12; i++) {
+                Console.WriteLine("");
                 Console.WriteLine("On the " + Days[i - 1] + " day of Christmas\nmy true love sent to me:");
-                if (i == 12) {
-                    Console.WriteLine("12 Drummers Drumming");
+                for (int j = (i-1); j >= 0; j--) {
+                    if (i > 1) Gifts[0] = "and a Partridge in a Pear Tree";
+                    Console.WriteLine(Gifts[j]);
                 }
-                if (i >= 11) {
-                    Console.WriteLine("11 Pipers Piping");
-                }
-                if (i >= 10) {
-                    Console.WriteLine("10 Lords a Leaping");
-                }
-                if (i >= 9) {
-                    Console.WriteLine("9 Ladies Dancing");
-                }
-                if (i >= 8) {
-                    Console.WriteLine("8 Maids a Milking");
-                }
-                if (i >= 7) {
-                    Console.WriteLine("7 Swans a Swimming");
-                }
-                if (i >= 6) {
-                    Console.WriteLine("6 Geese a Laying");
-                }
-                if (i >= 5) {
-                    Console.WriteLine("5 Golden Rings");
-                }
-                if (i >= 4) {
-                    Console.WriteLine("4 Calling Birds");
-                }
-                if (i >= 3) {
-                    Console.WriteLine("3 French Hens");
-                }
-                if (i >= 2) {
-                    Console.WriteLine("2 Turtle Doves");
-                }
-                if (i >= 1) {
-                    if (i == 1) {
-                        Console.WriteLine("1 Partridge in a Pear Tree\n");                        
-                    }
-                    else
-                        Console.WriteLine("and 1 Partridge in a Pear Tree\n");
-                }
+                Gifts[0] = "a Partridge in a Pear Tree";          
+                
             }
             Console.ReadLine();
         }
